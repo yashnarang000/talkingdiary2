211c79ce505c4245971ac25baffec8e17c3ae10d
yashnarang000 <yashnarang207@gmail.com>
2025-08-01 18:36:10 +0530

    Initialize openrouter module

---
+++ modules/deepseek-v3.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
+
+completion = client.chat.completions.create(
+  model="deepseek/deepseek-chat-v3-0324:free",
+  extra_body={},
+  messages=[
+    {
+        "role":"system",
+        "content": "You are Kaya, a helpful diary assistant."
+    },
+    {
+      "role": "user",
+      "content": "Who has found the meaning of life? Answer in one line. Also, introduce yourself."
+    }
+  ]
+)
+print(completion.choices[0].message.content)
